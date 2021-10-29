6dbfee378c6bb0900eba4e266234a68b5bf98ea4
abrahambtz <abrahambz@outlook.com>
2021-10-28 19:08:37 -0500

    Reporte de Nats

--- nat.py
+++ nat.py
@@ -1,8 +1,5 @@
 # 🤖 Abraham Baltazar Garcia Moreno 🐍
-
-
 import json
-from typing import ValuesView
 import pandas
 
 from pandas.io.json import json_normalize
@@ -82,86 +79,9 @@ def get_nats(rute):
     nat_listdict = filter_data_nat(nat_listdict)
     return nat_listdict
 
-    
-    
-    # lista_nat = nat_list
-    
-
-    # nat_data = nat_list
-    # objects = object_list
-    # nats_list = filter_ipv4('kondinero/json/nat.json')
-    # objects_list = filter_ipv4('kondinero/json/object.json')
-    # groups_list = filter_ipv4('kondinero/json/groups.json')
-
-    # service_groups_list = open_service('kondinero/json/service-groups.json')
-    # service_objects_list = filter_ipv4('kondinero/json/service-object.json')
-
-    # service_object = []
-    # service_group = []
-
-    # address_object = []
-    # address_group = []
-    # for object in objects:
-    #     for dict in groups_list:
-    #         if dict['name'] == object:
-    #             for key, value in dict.items():
-    #                 if key == 'address_object':
-    #                     address_object = [x['name'] for x in value['ipv4']]
-    #                     print('Grups:')
-    #                     print(address_object, end='\n\n')
-    #                 elif key == 'address_group':
-    #                     address_group = [x['name'] for x in value['ipv4']]
-    #                     print('Grups:')
-    #                     print(address_group, end='\n\n')
-    # values_objetcs = []
-    # objects_data = objects
-    # objects = objects + address_object + address_group
-    
-    # for object in objects:
-    #     for dict in objects_list:
-    #         if dict['name'] == object:
-
-    #             for key, value in dict.items():
-    #                 if not key == 'uuid':
-    #                     print(key + ' : ' + str(value))
-    #                     values_objetcs.append({key:value})
-    #             print()
-    # # print('hola')
-    # # print(service_groups_list)
-    # for object in objects:
-    #     for dict in service_groups_list:
-    #         for key, value in dict.items():
-    #             if dict['name'] == object:
-    #                 if key == 'service_object':
-    #                     service_object = [x['name'] for x in value]
-    #                     print('------- Service Object --------')
-    #                     for i in service_object:
-    #                         print(i)
-    #                 elif key == 'service_group':
-    #                     service_group = [x['name'] for x in value]
-    #                     print(' ------ Service Groups --------')
-    #                     for i in service_group:
-    #                         print(i)
-    # print(end='\n\n')
-    # #print_pdf(nat_data,objects_data, values_objetcs, service_object, service_group)
-    # #print(nat_data)
-    # # print(objects_data) # List
-    # # print(values_objetcs) # ListDict
-    # # print(service_object) # List    
-    # # print(service_group) # List
-    
-    # return(nat_data)
-
-
-    # Queda pendiente obtener los servicios de la lista service_objects_list. 
-    # Queda pendiente los servicios de server groups, recordar que server groups es un grupo de
-    # de servicios y debemos encontrar los servicios que sean de ese grupo. (Validacion)
-
-    # * Mejora de codigo *
-
-
 def run():
-    rute = 'kondinero/json/nat.json'
+    custumer = 'sisa'
+    rute = custumer+'/json/nat.json'
     nats = get_nats(rute)
     objects = get_objetcs_nat(nats)
     # for value in objects:
@@ -169,7 +89,7 @@ def run():
 
     nats = json_normalize(nats) 
     objects = json_normalize(objects)
-    with pandas.ExcelWriter('output.xlsx') as report: 
+    with pandas.ExcelWriter(custumer +'/output.xlsx') as report: 
         nats.to_excel(report, sheet_name='NAT')
         objects.to_excel(report, sheet_name='OBJECTS AND GROUPS')
     
